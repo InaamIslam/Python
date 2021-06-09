505b6f2b97377335a26309f6f3522fabf2e70845
InaamIslam <inaam.islaam@gmail.com>
2021-06-09 20:18:32 +0000

    testing files

---
+++ modules/diceapp.py
@@ -0,0 +1,12 @@
+# Create 2 new python files. In one file, 
+# write a function that will generate a number between 1 and 6.
+#  Make this a module called dice.
+# In the second file, use the dice module to simulate 
+# throwing 2 dice and return the values you get from the dice.
+
+def dice(number):
+    import random 
+    number = random.random(1,6)
+    print(number)
+
+number = 
---
+++ modules/modules.py
@@ -0,0 +1,15 @@
+####There are many procedures and functions built into
+####Python without any prerequisites needed. A simple example is print.
+####Modules ( or Libraries) are collections of extra, pre-written functions and procedures.
+####These are available to use, but we need to import them into our code at the start.
+
+import math
+
+number = float(input("Enter a number:"))
+answer = math.sqrt(number)
+
+print(answer)
+
+####
+
+
